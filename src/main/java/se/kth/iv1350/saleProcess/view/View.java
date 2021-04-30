0081cc337aa6c02ee4eeff793cf364724fe62250
Katrin Kahur <katrin.kahur@gmail.com>
2021-04-30 14:33:03 +0200

    View class is created

---
+++ src/main/java/se/kth/iv1350/saleProcess/view/View.java
@@ -0,0 +1,19 @@
+package se.kth.iv1350.saleProcess.view;
+
+import se.kth.iv1350.saleProcess.controller.Controller;
+
+/**
+ * This class is a placeholder for the entire view.
+ */
+public class View {
+    Controller contr;
+
+    /**
+     * Creates a new instance of the class.
+     * @param contr The controller that is used for all operations.
+     */
+    public View(Controller contr){
+        this.contr = contr;
+    }
+
+}
