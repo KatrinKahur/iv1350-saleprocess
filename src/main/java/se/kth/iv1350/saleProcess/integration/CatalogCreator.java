b56ef4b8e8499718be22becb187f84b82ad56ce2
Katrin Kahur <katrin.kahur@gmail.com>
2021-04-30 16:55:06 +0200

    Added the getters.

--- src/main/java/se/kth/iv1350/saleProcess/integration/CatalogCreator.java
+++ src/main/java/se/kth/iv1350/saleProcess/integration/CatalogCreator.java
@@ -4,4 +4,29 @@ package se.kth.iv1350.saleProcess.integration;
  * This class is the creator of all kinds of registries that are used in the program.
  */
 public class CatalogCreator {
+    private Inventory inventory = new Inventory();
+    private SaleCatalog saleCatalog = new SaleCatalog();
+    private CustomerCatalog customerCatalog = new CustomerCatalog();
+    private Accounting accounting = new Accounting();
+    private DiscountCatalog  discCatalog = new DiscountCatalog();
+
+    public Inventory getInventory(){
+        return inventory;
+    }
+
+    public SaleCatalog getSaleCatalog(){
+        return saleCatalog;
+    }
+
+    public CustomerCatalog getCustomerCatalog(){
+        return customerCatalog;
+    }
+
+    public Accounting getAccounting(){
+        return accounting;
+    }
+
+    public DiscountCatalog getDiscountCatalog(){
+        return discCatalog;
+    }
 }
