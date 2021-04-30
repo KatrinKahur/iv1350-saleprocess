b3be8a91459fd9a578eb3a95d5e45d81b27e7136
Katrin Kahur <katrin.kahur@gmail.com>
2021-04-30 14:33:26 +0200

    The main method is created

--- src/main/java/se/kth/iv1350/saleProcess/startup/Main.java
+++ src/main/java/se/kth/iv1350/saleProcess/startup/Main.java
@@ -1,4 +1,16 @@
 package se.kth.iv1350.saleProcess.startup;
 
+/**
+ * Starts the program. Contains the main method to start the program.
+ */
 public class Main {
+    /**
+     * The main method starts the program.
+     * @param args The method does not take any parameters.
+     */
+    public static void main (String[] args){
+
+
+
+    }
 }
