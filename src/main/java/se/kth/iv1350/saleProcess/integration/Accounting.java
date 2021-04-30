4b81beb6204ed70e826ab3a4814abcae06948dad
Katrin Kahur <katrin.kahur@gmail.com>
2021-04-30 15:35:51 +0200

    Added Accounting class.

---
+++ src/main/java/se/kth/iv1350/saleProcess/integration/Accounting.java
@@ -0,0 +1,7 @@
+package se.kth.iv1350.saleProcess.integration;
+
+/**
+ * This class contains calls to the external accounting system that has information about payments.
+ */
+public class Accounting {
+}
