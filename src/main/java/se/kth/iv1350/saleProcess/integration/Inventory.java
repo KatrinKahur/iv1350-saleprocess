8402a51580de49ea41792034a998b0e8fd37127d
Katrin Kahur <katrin.kahur@gmail.com>
2021-04-30 15:37:28 +0200

    Added Inventory class

---
+++ src/main/java/se/kth/iv1350/saleProcess/integration/Inventory.java
@@ -0,0 +1,7 @@
+package se.kth.iv1350.saleProcess.integration;
+
+/**
+ * This class contains calls to the external inventory system that has descriptions and prices of all grocery items.
+ */
+public class Inventory {
+}
