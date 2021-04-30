7ee6dd628077c2f5117d9fc96711bb8984b0d82a
Katrin Kahur <katrin.kahur@gmail.com>
2021-04-30 14:50:51 +0200

    Updated the Controller class with private variables.

--- src/main/java/se/kth/iv1350/saleProcess/controller/Controller.java
+++ src/main/java/se/kth/iv1350/saleProcess/controller/Controller.java
@@ -2,12 +2,14 @@ package se.kth.iv1350.saleProcess.controller;
 
 import se.kth.iv1350.saleProcess.integration.CatalogCreator;
 import se.kth.iv1350.saleProcess.integration.Printer;
+import se.kth.iv1350.saleProcess.model.CashRegister;
 
 /**
- * This is the program's only controller class. All method calls from the view to the model pass through this class.
+ * All method calls from the view to the model are passed through this class.
  */
 public class Controller {
     private CatalogCreator creator;
     private Printer printer;
+    private CashRegister cashRegister;
 
 }
