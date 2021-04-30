03a66b47148396167f65c8c9cea3b053ce77189e
Katrin Kahur <katrin.kahur@gmail.com>
2021-04-30 14:32:43 +0200

    Controller class is created.

---
+++ src/main/java/se/kth/iv1350/saleProcess/controller/Controller.java
@@ -0,0 +1,13 @@
+package se.kth.iv1350.saleProcess.controller;
+
+import se.kth.iv1350.saleProcess.integration.CatalogCreator;
+import se.kth.iv1350.saleProcess.integration.Printer;
+
+/**
+ * This is the program's only controller class. All method calls from the view to the model pass through this class.
+ */
+public class Controller {
+    private CatalogCreator creator;
+    private Printer printer;
+
+}
