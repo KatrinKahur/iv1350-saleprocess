1fcb75f62878baed7994f780a237fe37a6b10587
Katrin Kahur <katrin.kahur@gmail.com>
2021-05-02 16:07:54 +0200

    Added a constructor without quantity.

--- src/main/java/se/kth/iv1350/saleProcess/integration/ItemIdentifier.java
+++ src/main/java/se/kth/iv1350/saleProcess/integration/ItemIdentifier.java
@@ -4,13 +4,21 @@ package se.kth.iv1350.saleProcess.integration;
  * This class represents an item identifier of <code>Item</code>.
  */
 public class ItemIdentifier {
-    private int barcode;
+    private final int barcode;
     private int quantity;
 
     /**
      * Creates an instance of <code>ItemIdentifier</code>
      * @param barCode The identification code of an <code>Item</code>
-     * @param quantity The quantity of an <code>Item</code>
+     */
+    public ItemIdentifier(int barCode){
+        this.barcode = barCode;
+    }
+
+    /**
+     * Creates an instance of <code>ItemIdentifier</code>
+     * @param barCode The identification code of an <code>Item</code>
+     * @param quantity The quantity of the scanned item
      */
     public ItemIdentifier(int barCode, int quantity){
         this.barcode = barCode;
@@ -25,10 +33,6 @@ public class ItemIdentifier {
         return barcode;
     }
 
-    /**
-     * This class gets the value of <code>quantity</code>
-     * @return The value of <code>quantity</code>
-     */
     public int getQuantity(){
         return quantity;
     }
