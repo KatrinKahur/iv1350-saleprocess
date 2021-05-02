95cb7c21638a564ac5598e06d7a0375039c3d11c
Katrin Kahur <katrin.kahur@gmail.com>
2021-05-02 16:08:29 +0200

    Added the registerItem() method.

--- src/main/java/se/kth/iv1350/saleProcess/model/Sale.java
+++ src/main/java/se/kth/iv1350/saleProcess/model/Sale.java
@@ -1,5 +1,8 @@
 package se.kth.iv1350.saleProcess.model;
 
+import se.kth.iv1350.saleProcess.integration.ItemDTO;
+import se.kth.iv1350.saleProcess.integration.SaleDTO;
+
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -23,6 +26,7 @@ public class Sale {
         setSaleTime();
         receipt = new Receipt(saleTime);
         listOfItems = new ArrayList<>();
+        runningTotal = new Amount();
     }
 
     /**
@@ -31,13 +35,19 @@ public class Sale {
     private void setSaleTime(){
         saleTime = LocalDateTime.now();
     }
+    /**
+     * This method updates the running total.
+     */
+    private void updateRunningTotal(){
+        runningTotal = runningTotal.plus(recentlyScannedItem.getPriceWithVAT());
+    }
 
     /**
      * This method gets the value of <code>runningTotal</code>
      * @return The value of <code>runningTotal</code>
      */
     public Amount getRunningTotal(){
-        return new Amount(runningTotal.amount);
+        return runningTotal;
     }
 
     /**
@@ -48,4 +58,59 @@ public class Sale {
         return recentlyScannedItem;
     }
 
+    public SaleDTO registerItem(ItemDTO foundItem){
+        Item existingItem = checkForExistingItem(foundItem);
+
+        if (existingItem == null) {
+            setRecentlyScannedItem(new Item(foundItem));
+            addRecentlyScannedItemToTheItemList();
+        }
+        else {
+            existingItem.increaseQuantity();
+            setRecentlyScannedItem(existingItem);
+        }
+
+        recentlyScannedItem.calculatePriceWithVAT();
+        updateRunningTotal();
+        return createSaleInformation();
+    }
+
+    /**
+     * This method iterates the <code>listOfItems</code> to check if an <code>Item</code> with similar <code>barcode</code> already
+     * exists in <code>Sale</code>
+     * @param foundItem Used to get the scanned items barcode
+     * @return If <code>Item</code> with similar barcode already exists in <code>Sale</code>, the value of
+     * <code>Item</code> is returned, or else the program returns <code>null</code>
+     */
+    private Item checkForExistingItem(ItemDTO foundItem){
+        for (Item item : listOfItems){
+            if (foundItem.getBarcode() == item.getBarcode())
+                return item;
+        }
+        return null;
+    }
+
+    /**
+     * This method creates a new instance of <code>Item</code> and assigns <code>recentlyScannedItem</code>
+     * refer to it.
+     * @param newItem The fetched item from the inventory system
+     */
+    private void setRecentlyScannedItem(Item newItem){
+        recentlyScannedItem = newItem;
+    }
+
+    /**
+     * This method adds the most recently scanned item to the item list.
+     */
+    private void addRecentlyScannedItemToTheItemList(){
+        listOfItems.add(recentlyScannedItem);
+    }
+
+    /**
+     * This method creates a new instance of <code>SaleDTO</code> for the view.
+     * @return <code>SaleDTO</code> that includes item description, VAT and running total
+     */
+    public SaleDTO createSaleInformation(){
+        return new SaleDTO(this);
+    }
 }
